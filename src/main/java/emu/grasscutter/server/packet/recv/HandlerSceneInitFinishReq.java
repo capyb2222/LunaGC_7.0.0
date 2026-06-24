d6efee331756cafce1bde0ba63cee2185e7ea747
LP <luiz.grande@foursys.com.br>
2026-06-24 03:47:44 -0300

    Fixed issue: dungeon exit UI overlay on overworld load

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerSceneInitFinishReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerSceneInitFinishReq.java
@@ -20,7 +20,7 @@ public class HandlerSceneInitFinishReq extends PacketHandler {
         session.send(new PacketWorldPlayerInfoNotify(world));
         session.send(new PacketWorldDataNotify(world));
         session.send(new PacketPlayerWorldSceneInfoListNotify(player));
-        session.send(new BasePacket(PacketOpcodes.SceneForceUnlockNotify));
+        session.send(new PacketSceneForceUnlockNotify(1, true));
         session.send(new PacketHostPlayerNotify(world));
         session.send(new PacketSceneDataNotify(player.getSceneId()));
 
