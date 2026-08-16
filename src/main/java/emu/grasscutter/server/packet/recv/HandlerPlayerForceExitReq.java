2a1102e876c7818594d37634edb3738b64a5ab0e
capyb2222 <capyb2222@gmail.com>
2026-08-16 15:52:49 -0700

    fix(session): logout spawned a thread to sleep

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerForceExitReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerForceExitReq.java
@@ -2,6 +2,8 @@ package emu.grasscutter.server.packet.recv;
 
 import emu.grasscutter.net.packet.*;
 import emu.grasscutter.server.game.GameSession;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Opcodes(PacketOpcodes.PlayerForceExitReq)
 public class HandlerPlayerForceExitReq extends PacketHandler {
@@ -9,17 +11,8 @@ public class HandlerPlayerForceExitReq extends PacketHandler {
     public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
         // Client should auto disconnect right now
         session.send(new BasePacket(PacketOpcodes.PlayerForceExitRsp));
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000); // disconnect after 1 seconds
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                session.close();
-                super.run();
-            }
-        }.start();
+        // Was a whole thread spawned per logout just to wait a second before closing
+        CompletableFuture.runAsync(
+                session::close, CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS));
     }
 }
