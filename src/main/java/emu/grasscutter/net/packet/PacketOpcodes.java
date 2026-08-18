4db1fec1c5ace06d4d721b7426067664cb279065
capyb2222 <capyb2222@gmail.com>
2026-08-17 18:17:30 -0700

    fix(maplayer): entering a map layer got no reply

--- src/main/java/emu/grasscutter/net/packet/PacketOpcodes.java
+++ src/main/java/emu/grasscutter/net/packet/PacketOpcodes.java
@@ -560,12 +560,12 @@ public final class PacketOpcodes {
     public static final int UnlockTransPointRsp = 1776;
     public static final int PlayerEnterChildMapLayerNotify = -49; // no 7.0 entry
     public static final int PlayerEnterMapLayerNotify = -50; // no 7.0 entry
-    public static final int EnterMapLayerReq = -53; // no 7.0 entry
-    public static final int EnterMapLayerRsp = -54; // no 7.0 entry
+    public static final int EnterMapLayerReq = 4842;
+    public static final int EnterMapLayerRsp = 8901;
     public static final int EnterMapLayerReq_660 = -53; // no 7.0 entry
     public static final int EnterMapLayerRsp_660 = -54; // no 7.0 entry
-    public static final int UnlockMapLayerGroupReq = -103; // no 7.0 entry
-    public static final int UnlockMapLayerGroupRsp = -104; // no 7.0 entry
+    public static final int UnlockMapLayerGroupReq = 24031;
+    public static final int UnlockMapLayerGroupRsp = 23724;
     public static final int ReunionBriefInfoReq = 27913;
     public static final int ReunionBriefInfoRsp = 27874;
     public static final int GetQuestLackingResourceReq = 8759;
